4ab12711a569e0c5a18afd783c73a565dea25bff
Shreyas Kapur <shreyaskapur8@gmail.com>
2018-05-04 01:37:51 -0400

    NMS Flush

--- plugin/src/io/github/revalo/ChunkyServer/FlusherCommand.java
+++ plugin/src/io/github/revalo/ChunkyServer/FlusherCommand.java
@@ -1,8 +1,11 @@
 package io.github.revalo.ChunkyServer;
 
+import org.bukkit.Chunk;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_12_R1.CraftChunk;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
 import org.bukkit.entity.Player;
 
 public class FlusherCommand implements CommandExecutor {
@@ -12,8 +15,8 @@ public class FlusherCommand implements CommandExecutor {
             Player player = (Player) commandSender;
 
             if (args[0] == "flush") {
-                player.getWorld().unloadChunk(player.getLocation().getChunk().getX(), player.getLocation().getChunk().getZ(),
-                        true, false);
+                Chunk chunk = player.getLocation().getChunk();
+                ((CraftWorld) player.getWorld()).getHandle().getChunkProviderServer().saveChunk(((CraftChunk) chunk).getHandle(), false);
             } else if(args[0] == "load") {
                 player.getLocation().getChunk().unload(false, false);
                 player.getLocation().getChunk().load(false);
